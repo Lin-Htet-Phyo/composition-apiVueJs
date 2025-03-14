e000ecd4b2fb2b83e2e735e2df2f193632ee1680
Lin-Htet-Phyo <linhtethtetphyo181688@gmail.com>
2025-03-14 14:27:30 +0630

    fetch getPost detail

--- src/composable/getPost.js
+++ src/composable/getPost.js
@@ -1,19 +1,26 @@
 import { ref } from "vue";
+import { getDoc , doc } from "firebase/firestore";
+import {db} from "../firebase/config"
 
 let getPost = (id) => {
     let post = ref(null);
     let error = ref("");
     let load = async () => {
         try {
+            const postRef = doc(db, "posts", id);
+            const res = await getDoc(postRef);
+            if (res.exists()){
+                post.value = res.data();
+            }
             // await new Promise((resolve, reject) => {
             //     setTimeout(resolve, 2000);
             // });
-            let response = await fetch("http://localhost:3000/posts/" + id);
-            if (response.status === 404) {
-                throw new Error("Not found url!");
-            }
-            let data = await response.json();
-            post.value = data;
+            // let response = await fetch("http://localhost:3000/posts/" + id);
+            // if (response.status === 404) {
+            //     throw new Error("Not found url!");
+            // }
+            // let data = await response.json();
+            // post.value = data;
         } catch (err) {
             error.value = err.message;
         }
