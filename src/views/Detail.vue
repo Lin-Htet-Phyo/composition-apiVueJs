64846068a760af942e7641086635207d035cebe2
Lin-Htet-Phyo <linhtethtetphyo181688@gmail.com>
2025-03-14 19:25:23 +0630

    delete post from firebase

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -4,9 +4,10 @@
     <p>{{ post.body }}</p>
     <div v-if="post.tags">
       <div v-for="tag in post.tags" :key="tag" class="pill">
-      {{ tag }}
+        {{ tag }}
+      </div>
     </div>
-    </div>
+    <button class="delete" @click="deletePost">Delete</button>
   </div>
   <div v-else>
     <Spinner></Spinner>
@@ -14,21 +15,31 @@
 </template>
 
 <script>
-import { useRoute } from 'vue-router';
-import Spinner from '../components/Spinner'
+import { useRoute } from "vue-router";
+import Spinner from "../components/Spinner";
 import getPost from "../composable/getPost";
+import { db } from "../firebase/config";
+import { doc, deleteDoc } from "firebase/firestore";
+import { useRouter } from "vue-router";
 
 export default {
   components: { Spinner },
   props: ["id"], // this.$route.params.id
-  setup(props) { 
+  setup(props) {
     let route = new useRoute();
+    let router = useRouter();
     let { post, error, load } = getPost(route.params.id);
     load();
-
+    let deletePost = async () => {
+      let postId = props.id;
+      const postRef = doc(db, "posts", postId);
+      await deleteDoc(postRef);
+      router.push("/");
+    };
     return {
       post,
       error,
+      deletePost,
     };
   },
 };
@@ -58,7 +69,7 @@ export default {
   z-index: -1;
   padding-right: 40px;
   left: -30px;
-  transform: rotateZ('-1deg');
+  transform: rotateZ("-1deg");
 }
 .pill {
   display: inline-block;
@@ -68,5 +79,8 @@ export default {
   padding: 8px;
   border-radius: 20px;
   font-size-adjust: 14px;
+}
+button.delete {
+  margin: 30px auto;
 }
 </style>
